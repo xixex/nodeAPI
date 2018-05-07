bb73e5808bf95f42ac4e0d45fcf6fba15c2a86b7
Misha <mikhailsontsav@gmail.com>
2018-05-07 21:42:03 +0300

    working...

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var mongoClient = require ('mongodb').MongoClient;
+var mongoClient = require('mongodb').MongoClient;
 
 var multer = require('multer'); // v1.0.5
 var upload = multer(); // for parsing multipart/form-data
@@ -12,66 +12,74 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
 
 var artists = [
-  {
-    id: 1,
-    name: 'Deep Purple'
-  }, {
-    id: 2,
-    name: 'Iron Maden'
-  }, {
-    id: 3,
-    name: 'Metallica'
-  }
+    {
+        id: 1,
+        name: 'Deep Purple'
+    }, {
+        id: 2,
+        name: 'Iron Maden'
+    }, {
+        id: 3,
+        name: 'Metallica'
+    }
 ];
 app.put('/artist/:id', function (req, res) {
-  console.log(req.params);
-  var artist = artists.find(function(artist) {
-    return artist.id === + req.params.id;
-  })
-  artist.name = req.body.name;
-  res.send(200);
+    console.log(req.params);
+    var artist = artists.find(function (artist) {
+        return artist.id === +req.params.id;
+    })
+    artist.name = req.body.name;
+    res.send(200);
 });
 
 app.delete('/artist/:id', function (req, res) {
-  artists = artists.filter(function (artist) {
-    return artist.id !== +req.params.id;
-  res.send(200);
-  });
+    artists = artists.filter(function (artist) {
+        return artist.id !== +req.params.id;
+        res.send(200);
+    });
 });
 
-app.post('/artist', function(req, res) {
-  var artist = {
-    name: req.body.name
-  }
-  // res.send(artist)
+app.post('/artist', function (req, res) {
+    var artist = {
+        name: req.body.name
+    }
+    db.collection('artists').insert(artist, function (err, res) {
+        if (err) {
+            console.log(err);
+            res.send(500);
+        }
+        else {
+            res.send(artist)
+        }
+    });
 
 })
 
-app.get('/', function(req, res) {
-  res.send('Hello API');
+app.get('/', function (req, res) {
+    res.send('Hello API');
 });
 
-app.get('/artist', function(req, res) {
-  res.send(artists);
+app.get('/artist', function (req, res) {
+    res.send(artists);
 });
 
-app.get('/artist/:id', function(req, res) {
-  console.log(req.params);
-  var artist = artists.find(function(artist) {
-    return artist.id === + req.params.id;
-  })
-  res.send(artist);
+app.get('/artist/:id', function (req, res) {
+    console.log(req.params);
+    var artist = artists.find(function (artist) {
+        return artist.id === +req.params.id;
+    })
+    res.send(artist);
 })
 
 
 mongoClient.connect('mongodb://localhost:27017/myapi', function (err, database) {
-  if (err){
-    return console.error(err);
-  }
-  db = database;
+    if (err) {
+        return console.error(err);
+    }
+    db = database;
 
-  app.listen(8000, function() {
-    console.log('Server is started on 8000')
-  });
+    app.listen(8000, function () {
+        console.log('Server is started on 8000')
+    });
 
 })
