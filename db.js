c2df09399154793a4b08d17fee56758fb1aa0c0c
Misha <mikhailsontsav@gmail.com>
2018-05-07 23:31:31 +0300

    updated db file

--- db.js
+++ db.js
@@ -2,19 +2,25 @@ var mongoCliend = require('mongodb').MongoClient;
 var state = {
     db: null
 };
-exports
-    .connect = function (url, done) {
-    if (state.db) {
-        return done();
-    }
-    mongoCliend.connect(url, function (err, db) {
-        if (err) {
-            console.log(err);
+
+module.exports =
+    {
+        connect: function (url, done) {
+            if (state.db) {
+                return done();
+            }
+            mongoCliend.connect(url, function (err, db) {
+                if (err) {
+                    console.log(err);
+                }
+                state.db = db;
+                done();
+            })
+        },
+
+        get: function () {
+            return state.db;
         }
-        state.db = db;
-        done();
-    })
-};
-exports.get = function () {
-    return state.db;
-};
+    }
+
+
