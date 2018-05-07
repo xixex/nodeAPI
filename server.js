6ec769a14b1677667b9e6d80ec4b3f4528578603
Misha <mikhailsontsav@gmail.com>
2018-05-07 21:39:28 +0300

    working...

--- server.js
+++ server.js
@@ -1,20 +1,51 @@
+'use strict'
 var express = require('express');
-var app = express()
+var app = express();
+var bodyParser = require('body-parser');
+var mongoClient = require ('mongodb').MongoClient;
+
+var multer = require('multer'); // v1.0.5
+var upload = multer(); // for parsing multipart/form-data
+var db;
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
 
 var artists = [
   {
     id: 1,
     name: 'Deep Purple'
-  },
-  {
+  }, {
     id: 2,
     name: 'Iron Maden'
-  },
-  {
+  }, {
     id: 3,
     name: 'Metallica'
   }
 ];
+app.put('/artist/:id', function (req, res) {
+  console.log(req.params);
+  var artist = artists.find(function(artist) {
+    return artist.id === + req.params.id;
+  })
+  artist.name = req.body.name;
+  res.send(200);
+});
+
+app.delete('/artist/:id', function (req, res) {
+  artists = artists.filter(function (artist) {
+    return artist.id !== +req.params.id;
+  res.send(200);
+  });
+});
+
+app.post('/artist', function(req, res) {
+  var artist = {
+    name: req.body.name
+  }
+  // res.send(artist)
+
+})
 
 app.get('/', function(req, res) {
   res.send('Hello API');
@@ -24,6 +55,23 @@ app.get('/artist', function(req, res) {
   res.send(artists);
 });
 
-app.listen(8000, function() {
-  console.log('Server is started on 8000')
-});
+app.get('/artist/:id', function(req, res) {
+  console.log(req.params);
+  var artist = artists.find(function(artist) {
+    return artist.id === + req.params.id;
+  })
+  res.send(artist);
+})
+
+
+mongoClient.connect('mongodb://localhost:27017/myapi', function (err, database) {
+  if (err){
+    return console.error(err);
+  }
+  db = database;
+
+  app.listen(8000, function() {
+    console.log('Server is started on 8000')
+  });
+
+})
